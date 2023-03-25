bd92a6d7875979e8fcae2a409d11e4d0a02bad93
Sav1030 <wangping51030@gmail.com>
2023-03-25 12:59:12 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <h1>Núcleo de la aplicación</h1>;
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const container = document.getElementById('root');
+
+createRoot(container).render(<App />);
