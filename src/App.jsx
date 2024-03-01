e0a1495cb4aea3a2a691fa2d3cffc08f61effb39
sav1030 <wangping51030@gmail.com>
2024-03-01 06:46:18 -0800

    feature/bug-changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,7 @@
-import React, { useState, useRef } from "react";
+import MainContainer from './components/main-container/MainContainer';
 
-declare global {
-  export class ImageCapture {
-    constructor(track: MediaStreamTrack);
-    grabFrame(): Promise<ImageBitmap>;
-    takePhoto(): Promise<Blob>;
-  }
-}
-
-function App() {
-  const [screenshotURL, setScreenshotURL] = useState<string>("");
-  const [webcamPhotoURL, setWebcamPhotoURL] = useState<string>("");
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const startExam = async () => {
-    // Screen Capture
-    const mediaStream: MediaStream = await navigator.mediaDevices.getDisplayMedia();
-    const track: MediaStreamTrack = mediaStream.getVideoTracks()[0];
-    const imageCapture: ImageCapture = new ImageCapture(track);
-    const screenshot: ImageBitmap = await imageCapture.grabFrame();
-    
-    const screenshotCanvas: HTMLCanvasElement = document.createElement("canvas");
-    screenshotCanvas.width = screenshot.width;
-    screenshotCanvas.height = screenshot.height;
-    screenshotCanvas.getContext("2d")!.drawImage(screenshot, 0, 0);
-
-    setScreenshotURL(screenshotCanvas.toDataURL());
-
-    // Webcam Photo
-    const webcamStream: MediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
-    const webcamTrack: MediaStreamTrack = webcamStream.getVideoTracks()[0];
-    const webcamCapture: ImageCapture = new ImageCapture(webcamTrack);
-    const webcamPhoto: Blob = await webcamCapture.takePhoto();
-
-    const URLCreator = window.URL || window.webkitURL;
-    setWebcamPhotoURL(URLCreator.createObjectURL(webcamPhoto));
-  };
-
-  return (
-    <div ref={divRef}>
-      <button onClick={startExam}>Start Examination</button>
-      <h2>Screen Capture:</h2>
-      {screenshotURL && <img src={screenshotURL} alt="Screen Capture" />}
-      <h2>Webcam Photo:</h2>
-      {webcamPhotoURL && <img src={webcamPhotoURL} alt="Webcam Capture" />}
-    </div>
-  );
-}
+const App = () => {
+	return <MainContainer />;
+};
 
 export default App;
